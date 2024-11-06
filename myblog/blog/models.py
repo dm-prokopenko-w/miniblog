08c19826d756d05d8834f079f4ba10d29b73333b
Dmytro Prokopenko <dm.prokopenko.w@gmail.com>
2024-11-06 23:06:29 +0200

    add admin

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField('Title post', max_length=100)
+    description = models.TextField('Description post')
+    author = models.CharField('Name author',max_length=100)
+    date = models.DateField('Data post')
+
+    def __str__(self):
+        return f'{self.title}, {self.author}'
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
